08a99ecb89de56275a83255cb4bb0b3aa14b1279
elimathieu123 <elimathieu123@gmail.com>
2025-01-29 08:49:31 -0500

    javascript

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btPlus = document.querySelector('.js-plus');
+
+const colTransport = document.querySelector('.conteneur-transport');
+btPlus.addEventListener('click', fouvreferme);
+
+function fouvreferme(){
+    colTransport.classList.toggle('col-transport-invisible');
+};
